33053c773d535f8cff84b5bf5f03701a77fbb1aa
Thomas Carr <9591402+htcarr3@users.noreply.github.com>
2023-07-22 16:42:49 -0400

    initial commit

---
+++ spec/router_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Router do
+  it "has a version number" do
+    expect(Router::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
